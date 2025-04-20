e3a52d660d86e6c2b03b161c0e739eab4e0bee94
Ertuğrul Kundak <ertugrulkundak@hotmail.com>
2025-04-21 01:17:42 +0300

    ...

--- KargoAdmin/Controllers/HomeController.cs
+++ KargoAdmin/Controllers/HomeController.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// HomeController.cs
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using KargoAdmin.Models;
-
+using Microsoft.AspNetCore.Identity;
 namespace KargoAdmin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Kullanıcı giriş yapmışsa Admin sayfasına yönlendir
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+
+            // Giriş yapmamışsa giriş sayfasına yönlendir
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
         }
 
         public IActionResult Privacy()
